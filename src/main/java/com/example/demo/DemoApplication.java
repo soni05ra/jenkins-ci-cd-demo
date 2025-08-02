b6dbc5ab5f01dd18c9a7eeb468b6e5c342c3282e
soni05ra <soni05ra@gmail.com>
2025-08-02 21:34:11 +0530

    war name changed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.demo")
+
 public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
