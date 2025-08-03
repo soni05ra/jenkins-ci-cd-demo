77a7e8968ccb8826da6263472afae05f2d086a14
soni05ra <soni05ra@gmail.com>
2025-08-03 15:11:51 +0530

    RequestMapping

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,9 +2,11 @@ package com.example.demo.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/jenkinsCiCd")
 public class HelloController {
 
     @GetMapping("/hello")
